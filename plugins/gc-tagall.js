e9e4bcd3123ce9223909a7e9bf1c35f2b14969fb
Maguit022 <122250295+Maguit022@users.noreply.github.com>
2024-03-24 15:51:39 -0500

    Add files via upload

---
+++ plugins/gc-tagall.js
@@ -0,0 +1,19 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+let pesan = args.join` `
+let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
+let teks = `*⺀👻Madrugar se hace más fácil contigo a mi lado⺀*\n\n🟩 ${oi}\n\n🟩 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+for (let mem of participants) {
+teks += `┊🐲 @${mem.id.split('@')[0]}\n`}
+teks += `👤𝐍𝐢𝐤𝐨𝐥𝐬𝐢𝐭𝐚 𝐁𝐨𝐭`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.tags = ['group']
+handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
+handler.admin = true
+handler.group = true
+export default handler
